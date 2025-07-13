b86ab787cb85d39967ee5dd7f06fb73ac9b3d9ac
Dabi ayu <dabiayu8@gmail.com>
2025-07-13 08:24:59 +0100

    arranged login and register

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -20,7 +20,7 @@ export default function Login({ user, onLogin, onError }) {
     setLoading(true);
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/token/', {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}token/`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password })
